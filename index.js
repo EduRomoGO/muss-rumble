5529e2a3c2a169c936f463dad8c3187c8f910d63
EduRomoGO <eduromogo@outlook.es>
2017-05-05 21:00:50 +0200

    Add function to update json file with some data

--- index.js
+++ index.js
@@ -9,5 +9,18 @@ module.exports = {
 	    else { return (dec >>> 0).toString(2); }
 	},
 
-	
+	// updates json file ({data: []}) from fileUrl with jsonData
+	updateJsonFile: function (fileUrl, jsonData) {
+		const fs = require('fs');
+
+		fs.readFile(fileUrl, 'utf8', function (err, fileData){
+			if (err) console.log(err);
+
+			const fileObj = JSON.parse(fileData); //now it an object
+			fileObj.data.push(jsonData); //add some data
+			const json = JSON.stringify(fileObj); //convert it back to json
+			fs.writeFile(fileUrl, json, 'utf8', function(){ console.log('updated file with!');}); // write it back 
+		});
+	}
+
 };
