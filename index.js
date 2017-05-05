c77edf3e580dd8f14169631f33981ffcf9605a92
EduRomoGO <eduromogo@outlook.es>
2017-05-05 20:50:56 +0200

    Add explanation to method

--- index.js
+++ index.js
@@ -2,9 +2,12 @@
 
 module.exports = {
 
+	// transforms a decimal number to binary
 	dec2bin: function (dec){
 		console.log('dec2bin');
 		if (dec > 0) { return '0' + (dec >>> 0).toString(2); }
 	    else { return (dec >>> 0).toString(2); }
-	}
+	},
+
+	
 };
