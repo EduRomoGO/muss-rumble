81101d69753b2892a77295e59414e1676a062ba2
EduRomoGO <eduromogo@outlook.es>
2019-05-05 18:14:45 +0200

    fix: dont run code until its moment by using functions instead of prop, use getDBName() as intended

--- mongo/MongoUrl.js
+++ mongo/MongoUrl.js
@@ -2,24 +2,26 @@
 
 const {getDBName} = require('./mongoDbName.js');
 
-function getDevUrl () {
+function getLocalDBUrl () {
     const mongoUrlBase = 'mongodb://localhost:27017/';
     const mongoUrlDBName = getDBName();
 
     return mongoUrlBase + mongoUrlDBName;
 }
 
-const environmentsUrls = {
-    test: getDevUrl() + 'Test',
-    development: getDevUrl(),
+const getEnvironmentUrls = () => ({
+    test: getLocalDBUrl(),
+    development: getLocalDBUrl(),
     production: process.env.MONGODB_URI
-};
+});
 
 function getUrl () {
+  const environmentsUrls = getEnvironmentUrls();
+
     if (Object.keys(environmentsUrls).includes(process.env.NODE_ENV)) {
         return environmentsUrls[process.env.NODE_ENV];
     } else {
-        console.error(`Not environment specified, NODE_ENV is not one of ${Object.keys(environmentsUrls)}`);
+        console.error(`Not environment specified, NODE_ENV should be one of ${Object.keys(environmentsUrls)}`);
         process.exit();
     }
 }
