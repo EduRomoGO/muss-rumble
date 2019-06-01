6f24cfb4fdfaf038b9afae016a05abb989786b8a
EduRomoGO <eduromogo@outlook.es>
2019-06-01 11:35:27 +0200

    feat: add functions

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 'use strict';
 
+const getRandomNumberFrom = (min, max) => {
+  return Math.floor(Math.random() * max) + min;
+};
+
 const main = {
 
 	// transforms a decimal number to binary
@@ -18,13 +22,27 @@ const main = {
 			const fileObj = JSON.parse(fileData); //now it an object
 			fileObj.data.push(jsonData); //add some data
 			const json = JSON.stringify(fileObj, null, 4); //convert it back to json
-			fs.writeFile(fileUrl, json, 'utf8', function(){ console.log('updated file with!');}); // write it back 
+			fs.writeFile(fileUrl, json, 'utf8', function(){ console.log('updated file with!');}); // write it back
 		});
-	}
+  },
+
+  inspectObject: (o,i) => {
+    if(typeof i=='undefined')i='';
+    if(i.length>50)return '[MAX ITERATIONS]';
+    var r=[];
+    for(var p in o){
+        var t=typeof o[p];
+        r.push(i+'"'+p+'" ('+t+') => '+(t=='object' ? 'object:'+inspect(o[p],i+'  ') : o[p]+''));
+    }
+    return r.join(i+'\n');
+  },
 
 };
 
+
 module.exports.dec2bin = main.dec2bin;
 module.exports.updateJsonFile = main.updateJsonFile;
+module.exports.inspectObject = main.inspectObject;
+module.exports.getRandomNumberFrom = getRandomNumberFrom;
 module.exports.encrypter = require('./encrypter.js');
 module.exports.mongoUtil = require('./mongo/mongoUtil.js');
