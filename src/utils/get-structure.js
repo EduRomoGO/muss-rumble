82e18f819bc0930d760565107aa5d52a77397640
EduRomoGO <eduromogo@outlook.es>
2021-12-28 16:43:44 +0100

    docs

--- src/utils/get-structure.js
+++ src/utils/get-structure.js
@@ -2,23 +2,23 @@ const { storeJsonInFile } = require("./store-json-in-file");
 const { traverse } = require("./traverse");
 const util = require("util");
 
-function getIndexFiles(files) {
-  let indexFiles = [];
-
+function getIndexFiles(files, result = []) {
   files.forEach((file) => {
     if (file.type === "dir") {
-      return getIndexFiles(file.files);
+      result.push(file);
+      return getIndexFiles(file.files, result.files);
     }
 
-    if (file.path.includes("index")) {
-      indexFiles.push(file);
+    if (file.file.includes("index")) {
+      result.push(file);
     }
   });
 
-  return indexFiles;
+  return result;
 }
 
 function getStructure() {
+  // const files = traverse("/Users/EduRomoGO/Dropbox/1.info/1.Dev");
   const files = traverse("/Users/EduRomoGO/Dropbox/1.info/0.Handle");
 
   const indexFiles = getIndexFiles(files);
